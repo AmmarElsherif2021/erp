e11d4b9c8852d04f37d991c3d818d0716b1ecf2d
Ammar <ammar.alshareef2021@outlook.com>
2023-12-24 23:32:09 +0200

    .

---
+++ consale/src/layout/AddBtn/AddBtn.jsx
@@ -0,0 +1,9 @@
+import './AddBtn.css';
+const AddBtn=(props)=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default AddBtn
--- consale/src/routes/Dashboard/Dashboard.jsx
+++ consale/src/routes/Dashboard/Dashboard.jsx
@@ -18,16 +18,38 @@ import axios from 'axios'
 import AccCard from "../../layout/cards/AccCard/AccCard";
 import { useEffect, useState } from "react";
 import data from './data/accounts.json';
-
+import AddBtn from '../../layout/AddBtn/AddBtn';
 const Dashboard = () => {
-  const [accounts, setAccounts] = useState([]);
-  useEffect(() => {
-      setAccounts([...data])
-  }, []);
-  useEffect(() => {console.log(`${accounts}-------- >  ${accounts} <-----------`)
-}, [accounts]);
-
   
+
+//accounts state carries accounts.json records
+    const [accounts, setAccounts] = useState([]);
+    useEffect(() => {
+        setAccounts([...data])
+    }, []);
+    useEffect(() => {console.log(`-------- >  ${accounts} <-----------`)
+    }, [accounts]);
+
+  //add account ------------------------------------------------------
+
+    //generate Id
+    function generateRandomId() {
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      }
+  const addAcc=(name,password,confirmPassword)=>{
+    setAccounts((prev)=>[...prev,
+        {
+            wid:generateRandomId(),
+            w_name:name,
+            password:password,
+            
+//To be continued..
+// write on json file
+//continue addBtn
+
+        }
+    ])
+  }
 
   return (
       <div className="route-content dashboard">
@@ -43,7 +65,9 @@ const Dashboard = () => {
                       style={{backgroundColor: account.theme}}
                   />
               )) : <div>No accounts found</div>}
+              <div><AddBtn/></div>
           </div>
+
           <div></div>
       </div>
   )

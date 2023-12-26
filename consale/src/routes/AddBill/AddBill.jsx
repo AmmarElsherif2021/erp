e80356dfed75c4fa27760aa051345de5c4fc26e2
Ammar <ammar.alshareef2021@outlook.com>
2023-12-26 15:49:46 +0200

    .

--- consale/src/routes/AddBill/AddBill.jsx
+++ consale/src/routes/AddBill/AddBill.jsx
@@ -2,8 +2,23 @@ import './AddBill.css';
 const AddBill=()=>{
     return(
         <div className="route-content add-bill">
-        <h1>Bill</h1>
+        <h1>Add new bill here</h1>
+        <div className='add-bill-sections'>
+        <div className='new-bill-section'>
+            <input className='search-input' type="text" name="search" placeholder='search items'/>
+            <div className='new-bill-space'>
+            <h2>... new added items</h2>
+            </div>
         </div>
+        <div className='old-bills-section'>
+            <input className='search-input' type="text" name="search" placeholder='search items'/>
+            <div className='old-bills-space'>
+             <h2> .. old bills here</h2>
+            </div>
+        </div>
+        </div>
+        </div>
+        
     )
 };
 export default AddBill

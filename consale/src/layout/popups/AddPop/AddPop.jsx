4aecfec90d1f38b400c9d738b93901ce9eca9e26
Ammar <ammar.alshareef2021@outlook.com>
2023-12-26 15:20:56 +0200

    .

--- consale/src/layout/popups/AddPop/AddPop.jsx
+++ consale/src/layout/popups/AddPop/AddPop.jsx
@@ -4,7 +4,44 @@ const AddPop=({cancelAddPop})=>{
     return(
         <div className='add-pop'>
             <button className='cancel-add-pop' onClick={()=>cancelAddPop()}><img className='cancel-icon' src={cancelIcon}/></button>
+            
+            <div className='pop-body'></div>
             <h1> Add account </h1>
+            <div className='add-account-form'>
+            <form>
+            <div><label className='form-label'>
+         
+                <input className='input' type="text" name="name"  placeholder='Set Name'/>
+            </label>
+            </div>
+
+            <div><label className='form-label'>
+               
+                <input className='input' type="password" name="password" placeholder='Set Password'/>
+            </label>
+            </div>
+
+            <div  className='form-label'><label>
+                
+                <input className='input' type="password" name="confirmPassword" placeholder='Confirm Password'/>
+            </label>
+            </div>
+
+            <div className='form-label'>
+            
+            <label>
+                Admin:
+                <input className='input' type="radio" value="Admin" name="role" />
+            </label> 
+            <label>
+                Worker:
+                <input className='input' type="radio" value="Worker" name="role" />
+            </label>
+            </div>
+
+            <div className='form-label'><input className='input' type="submit" value="Submit" /></div>
+        </form>
+            </div>
         </div>
     )
 }
--- consale/src/routes/Stock/Stock.jsx
+++ consale/src/routes/Stock/Stock.jsx
@@ -1,6 +1,89 @@
 import './Stock.css';
 const Stock =()=>{
+    const data = [
+        {
+            id: 'Example ID',
+            parameter: {
+                name: 'Example Name',
+                image: 'Example Image',
+                description: 'Example Description',
+                quantity_stock: 'Example Quantity Stock',
+                price_unit: 'Example Price Unit'
+            }
+        },
+        // Add more objects for more rows
+        {
+            id: 'Example ID',
+            parameter: {
+                name: 'Example Name',
+                image: 'Example Image',
+                description: 'Example Description',
+                quantity_stock: 'Example Quantity Stock',
+                price_unit: 'Example Price Unit'
+            }
+        },
+        {
+            id: 'Example ID',
+            parameter: {
+                name: 'Example Name',
+                image: 'Example Image',
+                description: 'Example Description',
+                quantity_stock: 'Example Quantity Stock',
+                price_unit: 'Example Price Unit'
+            }
+        },
+        {
+            id: 'Example ID',
+            parameter: {
+                name: 'Example Name',
+                image: 'Example Image',
+                description: 'Example Description',
+                quantity_stock: 'Example Quantity Stock',
+                price_unit: 'Example Price Unit'
+            }
+        }
+    ];
     return(
-    <div className="route-content stock"><h1>Stock</h1></div>
+    <div className="route-content stock">
+    <h1>Stock</h1>
+    <div className='stock-header'>
+    <button className='add-stock'>Add</button>
+    <input className='search-input' type="text" name="search" placeholder='search items'/>
+    </div>
+    <div className='stock-table'>
+     
+    <table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Description</th>
+            <th>Quantity Stock</th>
+            <th>Price Unit</th>
+        </tr>
+    </thead>
+    <tbody>
+        {data.map((item, index) => (
+            <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.parameter.name}</td>
+                <td>{item.parameter.image}</td>
+                <td>{item.parameter.description}</td>
+                <td>{item.parameter.quantity_stock}</td>
+                <td>{item.parameter.price_unit}</td>
+                <td>
+                 <button className='table-btn' style={{backgroundColor: "#ff5c33"
+                    }}>-</button>
+                 <button className='table-btn' style={{backgroundColor: "#00994d"
+                }}>edit</button>
+                </td>
+            </tr>
+        ))}
+    </tbody>
+</table>
+
+    </div>
+    </div>
 )}
 export default Stock

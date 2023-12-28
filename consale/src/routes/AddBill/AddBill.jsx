9ade026ca3a62c61d7672a8f63f8f6b6781df41a
Ammar <ammar.alshareef2021@outlook.com>
2023-12-28 17:15:22 +0200

    .

--- consale/src/routes/AddBill/AddBill.jsx
+++ consale/src/routes/AddBill/AddBill.jsx
@@ -2,21 +2,25 @@ import './AddBill.css';
 const AddBill=()=>{
     return(
         <div className="route-content add-bill">
-        <h1>Add new bill here</h1>
-        <div className='add-bill-sections'>
-        <div className='new-bill-section'>
-            <input className='search-input' type="text" name="search" placeholder='search items'/>
-            <div className='new-bill-space'>
-            <h2>... new added items</h2>
+            <h1>Add new bill here</h1>
+
+            <div className='add-bill-sections'>
+                <div className='new-bill-section'>
+                    <div className='section-header'> 
+                      <input className='search-input' type="text" 
+                      name="search" placeholder='search items'/>
+                    </div>
+                    <div className='new-bill-space'>
+                       <h2>... new added items</h2>
+                    </div>
+                </div>
+                <div className='old-bills-section'>
+                <div className='section-header'> <input className='search-input' type="text" name="search" placeholder='search items'/></div>
+                    <div className='old-bills-space'>
+                    <h2> .. old bills here</h2>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div className='old-bills-section'>
-            <input className='search-input' type="text" name="search" placeholder='search items'/>
-            <div className='old-bills-space'>
-             <h2> .. old bills here</h2>
-            </div>
-        </div>
-        </div>
         </div>
         
     )
